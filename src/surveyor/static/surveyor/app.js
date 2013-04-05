56c9abf8cb345dbf23221e8f0b26a480c4bfc338
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-04-05 15:20:56 -0400

    Move the spinner code into the router

--- src/surveyor/static/surveyor/app.js
+++ src/surveyor/static/surveyor/app.js
@@ -11,10 +11,39 @@ var Surveyor = Surveyor || {};
       ':placeId': 'placeForm'
     },
 
+    initialize: function(options) {
+      this.$content = $(options['content']);
+    },
+
+    showSpinner: function() {
+      var opts = {
+            lines: 13, // The number of lines to draw
+            length: 20, // The length of each line
+            width: 10, // The line thickness
+            radius: 30, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#000', // #rgb or #rrggbb
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+          },
+          spinner = new Spinner(opts).spin(this.$content[0]);
+    },
+
     placeList: function() {
       console.log('At the list');
+      this.showSpinner();
+
       if (S.placeCollection.size() > 0) {
         S.placeListView.render();
+        $content.html(S.placeListView.el);
       }
       // S.placeCollection.fetch({reset: true});
     },
