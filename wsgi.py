432d215ff3e7b661b452b5c5bb91b037ffcc6b18
Aaron Ogle <atogle@gmail.com>
2013-04-08 15:03:42 -0400

    Update settings location

--- wsgi.py
+++ wsgi.py
@@ -19,7 +19,7 @@ import sys
 CURR_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(CURR_DIR, 'src'))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
