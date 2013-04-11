62e0710e08e1b7a541b14a8119ea936a68dac1c7
Aaron Ogle <atogle@gmail.com>
2013-04-11 14:02:43 -0400

    Update how routes and view rendering works - Add function to init a place collection - Always show a view on route and let its model or collection handle render - Make generic load spinner to reuse in the view - Show a spinner when rendering a view when there is not data - Move place collection fetch from the map view to the top level - Don't geolocate on load

--- src/surveyor/static/surveyor/app.js
+++ src/surveyor/static/surveyor/app.js
@@ -11,22 +11,23 @@ var Surveyor = Surveyor || {};
       ':placeId': 'placeForm'
     },
 
+    initPlaceCollection: function() {
+      if (S.currentLocation) {
+        S.fetchNearbyPlaces(S.currentLocation.latlng.lat, S.currentLocation.latlng.lng);
+      } else {
+        S.mapView.map.locate();
+      }
+    },
+
     placeList: function() {
       console.log('At the list');
 
-      // Check whether we already have places...
-      if (S.placeCollection.size() > 0) {
-        S.placeListView.render();
-        S.contentView.showView(S.placeListView);
-        return;
-      }
+      S.contentView.showView(S.placeListView.render());
 
-      // If not, init geolocation
-      if (S.config.map.geolocation_enabled) {
-        S.mapView.initGeolocation();
+      // Init nearby places if we need them
+      if (S.placeCollection.size() === 0) {
+        this.initPlaceCollection();
       }
-
-      S.contentView.showSpinner();
     },
 
     placeForm: function(placeId) {
@@ -36,10 +37,14 @@ var Surveyor = Surveyor || {};
               model: place,
               template: S.placeFormTemplate
             });
-            placeFormView.render();
-            S.contentView.showView(placeFormView);
+            S.contentView.showView(placeFormView.render());
           };
       console.log('At the form for ' + placeId);
+
+      // Init nearby places if we need them
+      if (S.placeCollection.size() === 0) {
+        this.initPlaceCollection();
+      }
 
       // If the place is loaded and we already have a view for it...
       placeFormView = S.placeFormViews[placeId];
@@ -58,16 +63,15 @@ var Surveyor = Surveyor || {};
       // If the place is not yet loaded...
       S.placeCollection.add({id: placeId});
       place = S.placeCollection.get(placeId);
+
+      // Render an empty place with a spinner, it will update on model change
+      createPlaceFormView();
+
       place.fetch({
         data: {
           include_submissions: true
-        },
-        success: function() {
-          createPlaceFormView();
         }
       });
-
-      S.contentView.showSpinner();
     }
   });
 
@@ -75,28 +79,6 @@ var Surveyor = Surveyor || {};
     events: {
       'click a': 'navigate'
     },
-    showSpinner: function() {
-      var opts = {
-            lines: 13, // The number of lines to draw
-            length: 20, // The length of each line
-            width: 10, // The line thickness
-            radius: 30, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#000', // #rgb or #rrggbb
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: 'auto', // Top position relative to parent in px
-            left: 'auto' // Left position relative to parent in px
-          },
-          spinner = new Spinner(opts).spin(this.el);
-    },
-
     showView: function(view) {
       this.$el.html(view.el);
     },
@@ -111,7 +93,6 @@ var Surveyor = Surveyor || {};
     initialize: function() {
       this.template = this.options.template;
       this.collection.on('reset', this.render, this);
-      console.log('initialized');
     },
 
     prettifyTimes: function() {
@@ -123,10 +104,19 @@ var Surveyor = Surveyor || {};
     },
 
     render: function() {
-      var data = this.collection.toJSON(),
-          html = this.template({'places': data});
-      this.$el.html(html);
-      this.prettifyTimes();
+      var data, html;
+
+      if (this.collection.size() > 0) {
+        data = this.collection.toJSON();
+        html = this.template({'places': data});
+
+        this.$el.html(html);
+        this.prettifyTimes();
+      } else {
+        // Show a spinner
+        S.loadSpinner.spin(this.el);
+      }
+
       return this;
     }
   });
@@ -134,6 +124,7 @@ var Surveyor = Surveyor || {};
   S.PlaceFormView = Backbone.View.extend({
     initialize: function() {
       this.template = this.options.template;
+      this.model.on('change', this.render, this);
     },
 
     events: {
@@ -206,19 +197,31 @@ var Surveyor = Surveyor || {};
     },
 
     render: function() {
-      var placeData = this.model.toJSON(),
-          survey = this.model.responseCollection.first(),
-          surveyData = (survey ? survey.toJSON() : {}),
-          surveyConfig = S.config.survey,
-          html;
+     var placeData,
+         survey,
+         surveyData,
+         surveyConfig,
+         html;
 
-      _.each(surveyConfig.items, function(item) {
-        item.value = surveyData[item.name];
-      });
+      if (this.model.has('location')) {
+        placeData = this.model.toJSON();
+        survey = this.model.responseCollection.first();
+        surveyData = (survey ? survey.toJSON() : {});
+        surveyConfig = S.config.survey;
 
-      html = this.template({place: placeData, survey: surveyData, survey_config: surveyConfig});
-      this.$el.html(html);
-      this.updateConditionalFields();
+        _.each(surveyConfig.items, function(item) {
+          item.value = surveyData[item.name];
+        });
+
+        html = this.template({place: placeData, survey: surveyData, survey_config: surveyConfig});
+        this.$el.html(html);
+        this.updateConditionalFields();
+
+      } else {
+        // Show a spinner
+        S.loadSpinner.spin(this.el);
+      }
+
       return this;
     }
   });
@@ -238,7 +241,13 @@ var Surveyor = Surveyor || {};
 
       // Remove default prefix
       self.map.attributionControl.setPrefix('');
+
+      // If not, init geolocation
+      if (mapConfig.geolocation_enabled) {
+        this.initGeolocation();
+      }
     },
+
     initGeolocation: function() {
       var self = this,
           currentLocationMarkers = L.layerGroup([]).addTo(self.map);
@@ -246,15 +255,6 @@ var Surveyor = Surveyor || {};
       var onLocationError = function(evt) {
         var mapOptions = self.options.mapConfig.options,
             message;
-
-        // Fetch new data near this place
-        self.collection.fetch({
-          reset: true,
-          data: {
-            near: mapOptions.center.lat+','+mapOptions.center.lng
-          }
-        });
-        self.map.setView([mapOptions.center.lat, mapOptions.center.lng], mapOptions.zoom);
 
         switch (evt.code) {
           // Unknown
@@ -274,46 +274,33 @@ var Surveyor = Surveyor || {};
             message = 'It took too long to determine your location. Please try again.';
             break;
         }
+
+        self.trigger('locationerror', evt);
         window.alert(message);
       };
 
       var onLocationFound = function(evt) {
         var msg, radius;
 
-        if(!self.map.options.maxBounds || self.map.options.maxBounds.contains(evt.latlng)) {
-          // Fetch new data near this place
-          self.collection.fetch({
-            reset: true,
-            data: {
-              near: evt.latlng.lat+','+evt.latlng.lng
-            },
-            success: function() {
-              S.contentView.showView(S.placeListView);
-            }
-          });
+        self.map.fitBounds(evt.bounds);
 
-          self.map.fitBounds(evt.bounds);
+        radius = evt.accuracy / 2;
+        currentLocationMarkers.clearLayers();
 
-          radius = evt.accuracy / 2;
-          currentLocationMarkers.clearLayers();
+        currentLocationMarkers.addLayer(L.circleMarker(evt.latlng, {
+          radius: 4,
+          weight: 2,
+          color: '#06f',
+          fillOpacity: 1
+        }));
 
-          currentLocationMarkers.addLayer(L.circleMarker(evt.latlng, {
-            radius: 4,
-            weight: 2,
-            color: '#06f',
-            fillOpacity: 1
-          }));
+        currentLocationMarkers.addLayer(L.circle(evt.latlng, radius, {
+          weight: 1,
+          color: '#06f',
+          fillOpacity: 0.1
+        }));
 
-          currentLocationMarkers.addLayer(L.circle(evt.latlng, radius, {
-            weight: 1,
-            color: '#06f',
-            fillOpacity: 0.1
-          }));
-        } else {
-          msg = 'It looks like you\'re not in a place where we\'re collecting ' +
-            'data. I\'m going to leave the map where it is, okay?';
-          window.alert(msg);
-        }
+        self.trigger('locationfound', evt);
       };
 
       // Add the geolocation control link
@@ -324,11 +311,6 @@ var Surveyor = Surveyor || {};
       // Bind event handling
       this.map.on('locationerror', onLocationError);
       this.map.on('locationfound', onLocationFound);
-
-      // Go to the current location if specified
-      if (this.options.mapConfig.geolocation_onload) {
-        this.map.locate();
-      }
     },
     handleGeolocateClick: function(evt) {
       if (evt) {
